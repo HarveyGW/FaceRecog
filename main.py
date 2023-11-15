0733cb754d757c86b7a150b1b19786e3a93d8733
Parrot Live user <user@localhost.localdomain>
2023-11-15 01:44:22 +0000

    Update

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
 import face_recognition
 import cv2
+import pickle
+from datetime import datetime
+import requests
+from colorama import Fore, Style, init
 
+init()
 
 # Function to load images from subdirectories and create face encodings
 def load_known_faces(root_directory):
@@ -12,51 +17,73 @@ def load_known_faces(root_directory):
         if os.path.isdir(person_dir):
             for filename in os.listdir(person_dir):
                 if filename.endswith(".jpg") or filename.endswith(".png"):
-                    # Load an image
                     image_path = os.path.join(person_dir, filename)
                     image = face_recognition.load_image_file(image_path)
-                    # Get face encodings
                     encodings = face_recognition.face_encodings(image)
                     if encodings:
                         known_faces.append(encodings[0])
                         known_names.append(person_name)
     return known_faces, known_names
 
+# Function to check if new images have been added
+def is_new_image_added(directory, last_check_time):
+    for root, _, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if file.endswith((".jpg", ".png")) and os.path.getmtime(file_path) > last_check_time:
+                return True
+    return False
 
-known_faces, known_names = load_known_faces(".")
+# Function to save encoded faces
+def save_encoded_faces(file_path, data):
+    with open(file_path, 'wb') as f:
+        pickle.dump(data, f)
 
+# Function to load encoded faces
+def load_encoded_faces(file_path):
+    with open(file_path, 'rb') as f:
+        return pickle.load(f)
 
-# Function to recognize faces in an uploaded image
-def recognize_faces(test_image_path):
-    # Load the test image
+# Load or process faces
+cache_file = 'encoded_faces.pkl'
+last_check_time = os.path.getmtime(cache_file) if os.path.exists(cache_file) else 0
+if os.path.exists(cache_file) and not is_new_image_added('.', last_check_time):
+    known_faces, known_names = load_encoded_faces(cache_file)
+else:
+    known_faces, known_names = load_known_faces('.')
+    save_encoded_faces(cache_file, (known_faces, known_names))
+
+# Function to recognize faces and save the image
+def recognize_faces_and_save(test_image_path, output_image_path):
     test_image = face_recognition.load_image_file(test_image_path)
     face_locations = face_recognition.face_locations(test_image)
     face_encodings = face_recognition.face_encodings(test_image, face_locations)
+    person_recognized = False
 
-    for (top, right, bottom, left), face_encoding in zip(
-        face_locations, face_encodings
-    ):
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         matches = face_recognition.compare_faces(known_faces, face_encoding)
-        name = "Unknown"
-
+        name = Fore.RED + "Unknown" + Style.RESET_ALL  # Red color for 'Unknown'
         if True in matches:
             first_match_index = matches.index(True)
-            name = known_names[first_match_index]
+            name = Fore.GREEN + known_names[first_match_index] + Style.RESET_ALL  # Green color for recognized names
+            person_recognized = True
 
         cv2.rectangle(test_image, (left, top), (right, bottom), (0, 0, 255), 2)
-        cv2.putText(
-            test_image,
-            name,
-            (left + 6, bottom - 6),
-            cv2.FONT_HERSHEY_DUPLEX,
-            1.0,
-            (255, 255, 255),
-            1,
-        )
+        cv2.putText(test_image, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
 
-    cv2.imshow("Image", cv2.cvtColor(test_image, cv2.COLOR_RGB2BGR))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.imwrite(output_image_path, cv2.cvtColor(test_image, cv2.COLOR_RGB2BGR))
 
+    if person_recognized:
+        print(Fore.CYAN + "Querying API..." + Style.RESET_ALL)
+        print(name)
+        try:
+            response = requests.get(f"http://45.87.28.51:5000/attendance?ID={name}&Location={location}&present=true")
+            response.raise_for_status()
+            print(Fore.YELLOW + f"API Response: {response.text}" + Style.RESET_ALL)
+        except requests.RequestException as e:
+            print(Fore.RED + f"API request failed: {e}" + Style.RESET_ALL)
 
-recognize_faces("test.jpg")
+location = "Owen"
+test_subject = input(Fore.BLUE + "Enter Test File Name: " + Style.RESET_ALL)
+output_path = "output.jpg"
+recognize_faces_and_save(test_subject, output_path)
